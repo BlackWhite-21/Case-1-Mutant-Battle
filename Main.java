d8eb0fd26e0e026ad5859cfd3e833f9e28470fb4
Steven Corea <stevencorea2004@gmail.com>
2026-09-25 15:56:10 -0600

    Case-1-Mutant-Battle/util/Constantes.java

--- Main.java
+++ Main.java
@@ -1,11 +1,21 @@
+import javax.swing.SwingUtilities;
+
+import iu.Controller.BattleController;
+import iu.View.Interfase;
 
 /**
  * Programa principal: corre el juego completo.
  */
-
 public class Main {
 
     public static void main(String[] args) {
-        
+        SwingUtilities.invokeLater(() -> {
+            BattleController controller = new BattleController();
+            Interfase interfase = new Interfase(controller);
+            controller.setInterfase(interfase);
+
+            interfase.mostrarPantallaCompleta();
+            controller.iniciarBatalla(interfase.pedirTamEquipo());
+        });
     }
 }
