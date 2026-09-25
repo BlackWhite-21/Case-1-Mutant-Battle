b4f5fd667484c59380b5144b6aea0acf91383477
Martin <marfran@gamil.com>
2026-09-25 03:48:04 -0600

    Merge

--- Game/ConfiguracionBattleField.java
+++ Game/ConfiguracionBattleField.java
@@ -1,29 +1,27 @@
 package game;
 
-import mutantbattle.model.Cordenada;
-import mutantbattle.util.Constantes;
+import java.awt.Point;
+import java.util.Random;
+
+import util.Constantes;
 
 /**
  * ConfiguracionBattleField solo guarda datos y los valida. No mueve mutantes ni decide combates.
  */
 public class ConfiguracionBattleField {
-
-    private final Cordenada borde;
+	
+    private final Point borde;
     private final int tamEquipo;
     private final double radio;
 
-    public ConfiguracionBattleField(int tamEquipo) {
-        if (tamEquipo < Constantes.TAM_EQUIPO_MIN || tamEquipo > Constantes.TAM_EQUIPO_MAX) {
-            throw new IllegalArgumentException(
-                    "El tamaño del equipo debe estar entre " + Constantes.TAM_EQUIPO_MIN
-                    + " y " + Constantes.TAM_EQUIPO_MAX + ", se recibió: " + tamEquipo);
-        }
-        this.tamEquipo = tamEquipo;
-        this.borde = new Cordenada(Constantes.BORDE_X, Constantes.BORDE_Y);
+    public ConfiguracionBattleField() {
+		Random random = new Random();
+        this.tamEquipo = random.nextInt(Constantes.TAM_EQUIPO_MIN, Constantes.TAM_EQUIPO_MAX + 1);
+        this.borde = new Point(Constantes.BORDE_X, Constantes.BORDE_Y);
         this.radio = Constantes.RADIO;
     }
 
-    public Cordenada getBorde() {
+    public Point getBorde() {
         return borde;
     }
 
